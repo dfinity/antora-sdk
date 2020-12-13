d6543e76f3407b828c96f5d9420fb59175e5b3fe
chenyan-dfinity <yan.chen@dfinity.org>
2020-12-12 20:55:51 -0800

    run button

--- src/js/vendor/run_repl.js
+++ src/js/vendor/run_repl.js
@@ -88,7 +88,7 @@ function appendRun(element, config) {
   if (config.isRun) {
     output.innerHTML = "<pre>Loading...</pre>";
   }
-  button.innerHTML = "▶ Run";
+  button.innerHTML = '<span class="run-label">Run</span><svg viewBox="0 0 24 24" preserveAspectRatio="xMidYMid meet" style="width: 35; height: 35;"><g><path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm-2 14.5v-9l6 4.5-6 4.5z"></path></g></svg>';
   button.classList = "run-button";
   parent.appendChild(button);
   parent.appendChild(output);
