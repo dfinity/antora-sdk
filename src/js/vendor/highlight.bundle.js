65d5070f8cbce48317ff52675587bf46e6efa309
Yan Chen <48968912+chenyan-dfinity@users.noreply.github.com>
2020-12-14 10:53:12 -0800

    Interpreter for language reference (#22)
    * bump version
    
    * attribute page.repl
    
    * page.moc-base-tag
    
    * button css
    
    * add syntax highlighting
    
    * more highlight
    
    * add codejar
    
    * add candid highlight
    
    * add bundle.zip
    
    * fix
    
    * add include
    
    * use moc.js from cdn
    
    * add bnf
    
    * run button

--- src/js/vendor/highlight.bundle.js
+++ src/js/vendor/highlight.bundle.js
@@ -2,66 +2,124 @@
   "use strict";
 
   var hljs = require("highlight.js/lib/highlight");
-  hljs.registerLanguage("apache", require("highlight.js/lib/languages/apache"));
-  hljs.registerLanguage(
-    "asciidoc",
-    require("highlight.js/lib/languages/asciidoc")
-  );
+
+  function registerMotoko() {
+    var string = {
+      className: 'string',
+      variants: [
+        {
+          begin: /r(#*)"(.|\n)*?"\1(?!#)/
+        },
+        {
+          begin: /b?'\\?(x\w{2}|u\w{4}|U\w{8}|.)'/
+        }
+      ]
+    };
+    var number = {
+      className: 'number',
+      variants: [
+        {
+          begin: '[+-]?\\b0[xX]([A-Fa-f0-9_]+)'
+        },
+        {
+          begin: '[+-]?\\b(\\d[\\d_]*(\\.[0-9_]+)?([eE][+-]?[0-9_]+)?)'
+        }
+      ],
+      relevance: 0
+    };
+    hljs.registerLanguage("motoko", function(hljs) {
+      return {
+        name: 'Motoko',
+        aliases: ['mo'],
+        keywords: {
+          $pattern: '[a-zA-Z_]\\w*',
+          keyword: 'actor and await break case catch class' +
+            ' continue debug else for func if in import' +
+            ' module not object or label let loop private' +
+            ' public return shared try throw query switch' +
+            ' type var while stable flexible system debug_show assert ignore',
+          literal: 'true false null',
+          built_in: 'Any None Null Bool Int Int8 Int16 Int32 Int64' +
+            ' Nat Nat8 Nat16 Nat32 Nat64 Word8 Word16 Word32 Word64' +
+            ' Float Char Text Blob Error Principal' +
+            ' async',
+        },
+        illegal: '</',
+        contains: [
+          hljs.C_LINE_COMMENT_MODE,
+          hljs.COMMENT('/\\*', '\\*/', {
+            contains: [ 'self' ]
+          }),
+          hljs.inherit(hljs.QUOTE_STRING_MODE, {
+            begin: /b?"/,
+            illegal: null
+          }),  
+          string,
+          number,
+          {
+            className: 'function',
+            beginKeywords: 'func',
+            end: '(\\(|<)',
+            excludeEnd: true,
+            contains: [ hljs.UNDERSCORE_TITLE_MODE ]
+          },
+          {
+            className: 'class',
+            begin: '\\b(actor(\ class)?|module|object)\\b',
+            keywords: 'actor class module object',
+            end: '(\\(|<|\{)',
+            contains: [ hljs.UNDERSCORE_TITLE_MODE ],
+            illegal: '[\\w\\d]'
+          },
+        ],
+      };
+    });
+    hljs.registerLanguage("candid", function(hljs) {
+      return {
+        name: 'Candid',
+        aliases: ['did'],
+        keywords: {
+          $pattern: '[a-zA-Z_]\\w*',
+          keyword: 'import service type',
+          built_in: 'opt vec record variant func blob principal' +
+            ' nat nat8 nat16 nat32 nat64 int int8 int16 int32 int64' +
+            ' float32 float64 bool text null reserved empty' +
+            ' oneway query',
+        },
+        illegal: '</',
+        contains: [
+          hljs.C_LINE_COMMENT_MODE,
+          hljs.COMMENT('/\\*', '\\*/', {
+            contains: [ 'self' ]
+          }),
+          hljs.inherit(hljs.QUOTE_STRING_MODE, {
+            begin: /b?"/,
+            illegal: null
+          }),  
+          string,
+          number,
+        ],
+      };
+    });
+  }
+  
   hljs.registerLanguage("bash", require("highlight.js/lib/languages/bash"));
-  hljs.registerLanguage(
-    "clojure",
-    require("highlight.js/lib/languages/clojure")
-  );
-  hljs.registerLanguage("cpp", require("highlight.js/lib/languages/cpp"));
-  hljs.registerLanguage("cs", require("highlight.js/lib/languages/cs"));
+  hljs.registerLanguage("bnf", require("highlight.js/lib/languages/bnf"));  
   hljs.registerLanguage("css", require("highlight.js/lib/languages/css"));
   hljs.registerLanguage("diff", require("highlight.js/lib/languages/diff"));
-  hljs.registerLanguage(
-    "dockerfile",
-    require("highlight.js/lib/languages/dockerfile")
-  );
-  hljs.registerLanguage("go", require("highlight.js/lib/languages/go"));
-  hljs.registerLanguage("groovy", require("highlight.js/lib/languages/groovy"));
-  hljs.registerLanguage(
-    "haskell",
-    require("highlight.js/lib/languages/haskell")
-  );
   hljs.registerLanguage("http", require("highlight.js/lib/languages/http"));
-  hljs.registerLanguage("ini", require("highlight.js/lib/languages/ini"));
-  hljs.registerLanguage("java", require("highlight.js/lib/languages/java"));
   hljs.registerLanguage(
     "javascript",
     require("highlight.js/lib/languages/javascript")
   );
   hljs.registerLanguage("json", require("highlight.js/lib/languages/json"));
-  hljs.registerLanguage("kotlin", require("highlight.js/lib/languages/kotlin"));
   hljs.registerLanguage(
     "makefile",
     require("highlight.js/lib/languages/makefile")
   );
-  hljs.registerLanguage(
-    "markdown",
-    require("highlight.js/lib/languages/markdown")
-  );
-  hljs.registerLanguage("nginx", require("highlight.js/lib/languages/nginx"));
-  hljs.registerLanguage("nix", require("highlight.js/lib/languages/nix"));
-  hljs.registerLanguage(
-    "objectivec",
-    require("highlight.js/lib/languages/objectivec")
-  );
-  hljs.registerLanguage("perl", require("highlight.js/lib/languages/perl"));
-  hljs.registerLanguage("php", require("highlight.js/lib/languages/php"));
-  hljs.registerLanguage(
-    "properties",
-    require("highlight.js/lib/languages/properties")
-  );
-  hljs.registerLanguage("python", require("highlight.js/lib/languages/python"));
-  hljs.registerLanguage("ruby", require("highlight.js/lib/languages/ruby"));
-  hljs.registerLanguage("scala", require("highlight.js/lib/languages/scala"));
+  hljs.registerLanguage("rust", require("highlight.js/lib/languages/rust"));  
   hljs.registerLanguage("shell", require("highlight.js/lib/languages/shell"));
-  hljs.registerLanguage("sql", require("highlight.js/lib/languages/sql"));
-  hljs.registerLanguage("swift", require("highlight.js/lib/languages/swift"));
-  hljs.registerLanguage("xml", require("highlight.js/lib/languages/xml"));
-  hljs.registerLanguage("yaml", require("highlight.js/lib/languages/yaml"));
+  registerMotoko();
   hljs.initHighlighting();
+  window.hljs = hljs;
 })();
---
+++ src/js/vendor/run_repl.js
@@ -0,0 +1,130 @@
+
+function changeCodeBlock() {
+  // Find all <pre> tags, check if they have a code tag as a direct child.
+  // If so, and the code class is Motoko, add run button, and make code editable.
+  // Checking first for pre tag
+  // ensures we don't add button to code blocks within paragraphs.
+  var preTags = document.getElementsByTagName("pre");
+  for (var i = 0; i < preTags.length; i++) {
+    var preTag = preTags[i];
+    var firstChild = preTag.firstChild;
+    if (firstChild && firstChild.tagName === "CODE") {
+      if (firstChild.classList.contains("language-motoko")) {
+        preTag.classList.add("motoko");
+        var config = extractConfig(preTag);
+        appendRun(preTag, config);
+      }
+    }
+  }
+}
+
+function extractConfig(pre) {
+  // Valid code block has <div class="listingblock"> as parent of parent,
+  // and we store the config info in the id and class attributes.
+  var div = pre.parentNode.parentNode;
+  var name = div.getAttribute("id");
+  if (name) {
+    name += ".mo";
+  }
+  var include = [];
+  var hook = null;
+  for (var i = 0; i < div.classList.length; i++) {
+    var config = div.classList[i];
+    if (config.startsWith("include")) {
+      var split = config.split("_");
+      for (var j = 1; j < split.length; j++) {
+        include.push(split[j]);
+      }
+    } else if (config.startsWith("hook")) {
+      var split = config.split("_");
+      hook = split[1];
+    }
+  }
+  return {
+    name: name,
+    include: include,
+    hook: hook,
+    isRun: div.classList.contains("run"),
+    noRepl: div.classList.contains("no-repl"),
+  };
+}
+
+function highlightCode(pre) {
+  var code_text = pre.firstChild.innerText;
+  // highlight.js is not very good at incremental changes. We need to reset the previous tags.
+  pre.firstChild.innerHTML = "";
+  pre.firstChild.textContent = code_text;
+  window.hljs.highlightBlock(pre);
+}
+
+function saveIncluded(include) {
+  var codes = {};
+  for (var i = 0; i < include.length; i++) {
+    var node = document.getElementById(include[i]);
+    var codeTag = node.querySelector("div.content").querySelector("pre").querySelector("code");
+    var code = codeTag.innerText;
+    var name = include[i] + ".mo";
+    Motoko.saveFile(name, code);
+    codes[name] = code;
+  }
+  return codes;
+}
+
+function appendRun(element, config) {
+  if (config.name) {
+    Motoko.saveFile(config.name, element.firstChild.innerText);
+  }
+  if (config.noRepl) {
+    return;
+  }
+  var jar = window.CodeJar(element, window.hljs.highlightBlock);
+  element.style = "";
+  
+  var parent = element.parentNode;
+  parent.style = "position:relative";
+  var button = document.createElement("button");
+  var output = document.createElement("div");
+  output.classList = "listingblock";
+  if (config.isRun) {
+    output.innerHTML = "<pre>Loading...</pre>";
+  }
+  button.innerHTML = '<span class="run-label">Run</span><svg viewBox="0 0 24 24" preserveAspectRatio="xMidYMid meet" style="width: 35; height: 35;"><g><path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm-2 14.5v-9l6 4.5-6 4.5z"></path></g></svg>';
+  button.classList = "run-button";
+  parent.appendChild(button);
+  parent.appendChild(output);
+  button.addEventListener("click", function () {
+    var codes = saveIncluded(config.include);
+    var code = element.firstChild.innerText;
+    var file = config.name || "stdin";
+    Motoko.saveFile(file, code);
+    var out;
+    if (config.hook) {
+      var fn = window[config.hook];
+      if (typeof fn !== "function") {
+        throw new Error(config.hook + " is not a function");
+      }
+      out = fn.apply(null, [codes, code]);
+    } else {
+      var list = config.include.map(function (s) { return s+".mo"});      
+      out = Motoko.run(list, file);
+    }
+    output.innerHTML = "";
+    if (out.stderr) {
+      var pre = document.createElement("pre");
+      pre.style = "color:red";
+      pre.innerText = out.stderr;
+      output.appendChild(pre);
+    }
+    if (out.stdout) {
+      var pre = document.createElement("pre");
+      pre.style = "color:green";
+      pre.innerText = out.stdout;
+      output.appendChild(pre);
+      //window.hljs.highlightBlock(pre);
+    }
+    highlightCode(element);
+  });
+  if (config.isRun) {
+    button.click();
+  }
+}
