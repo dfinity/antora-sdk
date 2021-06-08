95d5facca1f4428a5021f18eeba2cb5d9eb2dd37
Benjamin Goering <benjamin.goering@dfinity.org>
2021-06-08 09:30:59 -0700

    Revert "Update body-font-family to use CircularXX, add github workflow, fix build error (#39)"
    This reverts commit 53251898a57700711983d5f375d354f60ad7b788.

--- gulp.d/tasks/build.js
+++ gulp.d/tasks/build.js
@@ -16,9 +16,7 @@ const postcssCalc = require("postcss-calc");
 const postcssImport = require("postcss-import");
 const postcssUrl = require("postcss-url");
 const postcssVar = require("postcss-custom-properties");
-const uglifyes = require('uglify-es');
-const composer = require('gulp-uglify/composer');
-const uglify = composer(uglifyes, console);
+const uglify = require("gulp-uglify");
 const vfs = require("vinyl-fs");
 
 module.exports = (src, dest, preview) => () => {
@@ -73,8 +71,7 @@ module.exports = (src, dest, preview) => () => {
         })
       )
       .pipe(buffer())
-      .pipe(uglify())
-      ,
+      .pipe(uglify()),
     vfs
       .src("css/site.css", { ...opts, sourcemaps })
       .pipe(postcss(postcssPlugins)),
